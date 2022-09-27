87e4c8238d1ae44b98c68caa45d01546ca395189
Gder Code <gdercode@gmail.com>
2022-09-27 11:05:54 +0200

    final with db update

--- assets/Database/crime.sql
+++ assets/Database/crime.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 06, 2022 at 11:11 AM
+-- Generation Time: Sep 27, 2022 at 11:04 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -46,7 +46,7 @@ CREATE TABLE `all_user_table` (
 
 INSERT INTO `all_user_table` (`user_id`, `user_first_name`, `user_last_name`, `user_email`, `user_mobile`, `username`, `user_password`, `registration_date`, `user_update_date`, `user_role_id`) VALUES
 (1, 'superAdmin', 'superAdmin', 'superAdmin@gmail.com', '+250782546969', 'superAdmin', '$2y$10$psjPKzl2I4DauRYmSAnxUOA3Ev/Z5qrWPWQnPdM40TlF7qPhcuTJe', '2022-08-04 10:42:55', '0000-00-00 00:00:00', 1),
-(3, 'kagabo', 'pascal', 'kagabo@gmail.com', '+250782546968', 'kagabo', '$2y$10$E15wqK4vw0LPneJH94OO.u0fjijV/2Ea2y9NTZxAA3cgJZohdqdPS', '2022-08-04 20:28:58', '0000-00-00 00:00:00', 1);
+(3, 'kagabo', 'pascal', 'kagabo@gmail.com', '+250782546968', 'kagabo', '$2y$10$NvQStG65ERdzjcbqkxPx/.aNh8OBtQyzXSgwZTZNsnLsc6sxwWvqq', '2022-08-04 20:28:58', '0000-00-00 00:00:00', 1);
 
 -- --------------------------------------------------------
 
@@ -78,8 +78,24 @@ CREATE TABLE `testimony_table` (
   `t_id` int(11) NOT NULL,
   `u_id` int(11) NOT NULL,
   `w_id` int(11) NOT NULL,
-  `testimony` text NOT NULL
+  `testimony` text NOT NULL,
+  `testimony_date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `testimony_table`
+--
+
+INSERT INTO `testimony_table` (`t_id`, `u_id`, `w_id`, `testimony`, `testimony_date`) VALUES
+(1, 1, 10, 'uyu ntakintu muziho', '2022-09-08 11:38:48'),
+(2, 1, 10, 'uyu jewe ndamuzi ararengana ', '2022-09-08 11:45:40'),
+(3, 1, 10, 'uyu ntahave', '2022-09-08 11:46:58'),
+(4, 1, 10, 'nibyo', '2022-09-08 11:47:35'),
+(5, 1, 10, 'nibyo', '2022-09-08 11:52:32'),
+(6, 3, 10, 'inyangamugayo', '2022-09-09 08:53:13'),
+(7, 1, 12, 'Uyu ndamuzi ni mubi cyane \r\n', '2022-09-12 10:55:59'),
+(8, 1, 12, 'hhhh', '2022-09-12 10:56:16'),
+(9, 1, 13, 'Uyu ndamuzi n umujura ', '2022-09-27 10:46:14');
 
 -- --------------------------------------------------------
 
@@ -94,6 +110,16 @@ CREATE TABLE `wanted_table` (
   `wanted_gender` varchar(6) DEFAULT NULL,
   `wanted_age` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `wanted_table`
+--
+
+INSERT INTO `wanted_table` (`wanted_id`, `wanted_First_name`, `wanted_last_name`, `wanted_gender`, `wanted_age`) VALUES
+(10, 'Gakuba', 'Paul', 'Male', 32),
+(11, 'uwase', 'Pascaline', 'female', 21),
+(12, 'gakubaf', 'paulf', 'male', 55),
+(13, 'butoya', 'kabange', 'male', 40);
 
 --
 -- Indexes for dumped tables
@@ -146,13 +172,13 @@ ALTER TABLE `role_table`
 -- AUTO_INCREMENT for table `testimony_table`
 --
 ALTER TABLE `testimony_table`
-  MODIFY `t_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `t_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `wanted_table`
 --
 ALTER TABLE `wanted_table`
-  MODIFY `wanted_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `wanted_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- Constraints for dumped tables
